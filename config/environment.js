c395c92fdb6b929b9dab8525f17d2d7fb02d9298
Christopher Truett <christophertruett@christohersmbp2.home>
2016-03-18 06:34:40 -0400

    Cleaned everything up.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ticker',
+    podModulePrefix: 'ticker/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
